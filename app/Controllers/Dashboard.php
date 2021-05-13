9ddd24aed154fc7d4c590a0d690f94922d096cc5
ihkaru <ihza2karunia@gmail.com>
2021-05-14 03:03:42 +0700

    Implementasi layout

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Dashboard extends BaseController
+{
+	public function index()
+	{
+		return view('dash/index');
+	}
+
+	public function covid(){
+
+	}
+
+	public function ekonomi()
+	{
+		
+	}
+
+}
